1dd71c9cebcd6cf3aa0c3456293130dfd2df7747
ilia-pw <iliabelkania91@gmail.com>
2025-07-24 21:51:00 +0400

    second commit

---
+++ day22/homework/lessone1.py
@@ -0,0 +1,7 @@
+
+# Conditional statements / პირობითი განცხადებე
+# Keywords if, elif, else
+ # if - თუ
+ # elif - სრული ვერსია else if else - სხვა შემთხვევაში if - საჭიროებს პირობას
+ # elif - საჭიროებს პირობას
+ # else - არ საჭიროებს პირობას
